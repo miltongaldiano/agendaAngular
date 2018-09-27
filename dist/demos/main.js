dc506f461976e60397cdf780cc1bf147ab094c9b
Milton <miltongaldiano@gmail.com>
2018-09-27 20:54:47 -0300

    Retirar https

--- dist/demos/main.js
+++ dist/demos/main.js
@@ -5430,7 +5430,7 @@ var DemoComponent = /** @class */ (function () {
     DemoComponent.prototype.doctors = function () {
         var _this = this;
         this.http
-            .get('https://ec2-54-145-167-89.compute-1.amazonaws.com/api/medicos')
+            .get('http://ec2-54-145-167-89.compute-1.amazonaws.com/api/medicos')
             .toPromise()
             .then(function (resp) {
             _this.doctorss = resp.json().data.map(function (element) {
@@ -5444,7 +5444,7 @@ var DemoComponent = /** @class */ (function () {
     DemoComponent.prototype.patients = function () {
         var _this = this;
         this.http
-            .get("https://ec2-54-145-167-89.compute-1.amazonaws.com/api/pacientes")
+            .get("http://ec2-54-145-167-89.compute-1.amazonaws.com/api/pacientes")
             .toPromise()
             .then(function (resp) {
             _this.patientss = resp.json().data.map(function (element) {
@@ -5460,7 +5460,7 @@ var DemoComponent = /** @class */ (function () {
         var _this = this;
         this.events = [];
         this.http
-            .get("https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas?medico_id=" + this.doctor1)
+            .get("http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas?medico_id=" + this.doctor1)
             .toPromise()
             .then(function (resp) {
             resp.json().data.forEach(function (element) {
@@ -5486,7 +5486,7 @@ var DemoComponent = /** @class */ (function () {
     DemoComponent.prototype.delete = function (id) {
         var _this = this;
         this.http
-            .delete("https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas/" + id)
+            .delete("http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas/" + id)
             .toPromise()
             .then(function (resp) {
             _this.commitments();
@@ -5526,7 +5526,7 @@ var DemoComponent = /** @class */ (function () {
         }
         else {
             this.http
-                .post("https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas", {
+                .post("http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas", {
                 datahora: this.dateEvent,
                 medico_id: this.doctor1,
                 paciente_id: this.patient1
--- src/app/demo-modules/kitchen-sink/component.ts
+++ src/app/demo-modules/kitchen-sink/component.ts
@@ -151,7 +151,7 @@ export class DemoComponent implements OnInit {
 
   doctors() {
     this.http
-      .get('https://ec2-54-145-167-89.compute-1.amazonaws.com/api/medicos')
+      .get('http://ec2-54-145-167-89.compute-1.amazonaws.com/api/medicos')
       .toPromise()
       .then(resp => {
         this.doctorss = resp.json().data.map(element => {
@@ -165,7 +165,7 @@ export class DemoComponent implements OnInit {
 
   patients() {
     this.http
-      .get(`https://ec2-54-145-167-89.compute-1.amazonaws.com/api/pacientes`)
+      .get(`http://ec2-54-145-167-89.compute-1.amazonaws.com/api/pacientes`)
       .toPromise()
       .then(resp => {
         this.patientss = resp.json().data.map(element => {
@@ -181,7 +181,7 @@ export class DemoComponent implements OnInit {
   commitments() {
     this.events = [];
     this.http
-      .get(`https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas?medico_id=${this.doctor1}`)
+      .get(`http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas?medico_id=${this.doctor1}`)
       .toPromise()
       .then(resp => {
         resp.json().data.forEach(element => {
@@ -208,7 +208,7 @@ export class DemoComponent implements OnInit {
 
   delete(id: number) {
     this.http
-      .delete(`https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas/${id}`)
+      .delete(`http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas/${id}`)
       .toPromise()
       .then(resp => {
         this.commitments();
@@ -255,7 +255,7 @@ export class DemoComponent implements OnInit {
     } else {
 
       this.http
-      .post(`https://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas`, {
+      .post(`http://ec2-54-145-167-89.compute-1.amazonaws.com/api/agendas`, {
         datahora: this.dateEvent,
         medico_id: this.doctor1,
         paciente_id: this.patient1
